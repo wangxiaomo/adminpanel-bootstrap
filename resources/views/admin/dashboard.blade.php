46e60efc0f412ed5bc3198ac8c5b3568184bee30
wangxiaomo <wxm4ever@gmail.com>
2017-08-07 20:55:23 +0800

    update super admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,11 +12,20 @@
   <section class="content-header">
     <h1>Page Header<small>Optional description</small></h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-      <li class="active">Here</li>
+      <li class="active">Dashboard</li>
     </ol>
   </section>
   <section class="content container-fluid">
-    | Your Page Content Here |
+    <div class="row">
+      <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+          <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
+          <div class="info-box-content">
+            <span class="info-box-text">Likes</span>
+            <span class="info-box-number">93,139</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 @endsection
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,13 +1,12 @@
 @if (request()->admin_user->admin_type == \App\Models\AdminUser::SUPER_ADMIN)
   <section class="sidebar">
     <ul class="sidebar-menu tree" data-widget="tree">
-      <li class="header">Header</li>
-      <li class="active"><a href="#"><i class="fa fa-link"></i><span>link</span></a></li>
+      <li class="active"><a href="/admin"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
       <li class="treeview">
-        <a href="#"><i class="fa fa-link"></i><span>Multi</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
+        <a href="#"><i class="fa fa-group"></i><span>管理员管理</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
         <ul class="treeview-menu" style="display:none">
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
+          <li><a href="/admin/users"><i class="fa fa-circle-o"></i>管理员信息</a></li>
+          <li><a href="/admin/users/create"><i class="fa fa-circle-o"></i>新建管理员</a></li>
         </ul>
       </li>
     </ul>
